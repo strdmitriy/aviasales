8cb02d06bb2717dff54268e33b7d1455009f2faa
Дмитрий Струнгарь <dmitrijstrungar@Dmitrijs-MacBook-Pro.local>
2020-07-22 10:29:49 +0500

    init project

---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin").CleanWebpackPlugin;
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+    watchContentBase: true,
+    progress: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(svg|woff|woff2|ttf|eot|otf)([\?]?.*)$/,
+        loader: "file-loader?name=assets/fonts/[name].[ext]"
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === "development"
+            }
+          },
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".jsx", ".tsx", ".ts", ".js"]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "src/index.html",
+          to: "./index.html"
+        },
+        {
+          from: "src/assets/**/*",
+          to: "./assets",
+          transformPath(targetPath, absolutePath) {
+            return targetPath.replace("src/assets", "");
+          }
+        }
+      ]
+    }),
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true
+      }
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style-[hash].css",
+      allChunks: true
+    })
+  ]
+};
