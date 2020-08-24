aa4dd12efbc979f0ca4a1752c2d5b04c3d58b852
Дмитрий Струнгарь <dmitrijstrungar@Dmitrijs-MacBook-Pro.local>
2020-08-24 10:01:10 +0500

    feature-widgets

--- jest.config.js
+++ jest.config.js
@@ -64,10 +64,10 @@ module.exports = {
   // maxWorkers: "50%",
 
   // An array of directory names to be searched recursively up from the requiring module's location
-  "moduleDirectories": [
-    "node_modules",
-    "src"
-],
+   moduleDirectories: [
+     "node_modules",
+     "src"
+   ],
 
   // An array of file extensions your modules use
   // moduleFileExtensions: [
@@ -80,9 +80,9 @@ module.exports = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  moduleNameMapper: {
+   moduleNameMapper: {
     ".+\\.(svg|png|jpg)$": "identity-obj-proxy"
-  },
+   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -115,7 +115,7 @@ module.exports = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  rootDir: undefined,
+  // rootDir: undefined,
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
@@ -138,7 +138,7 @@ module.exports = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "jsdom",
+  testEnvironment: "jsdom"
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
@@ -173,7 +173,7 @@ module.exports = {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-  // transform,
+  // transform: undefined,
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
